e83eaf12e5f96c14fdb5a8e3c984b878154d4cd3
Azam Din <adin786@gmail.com>
2021-06-28 23:51:39 +0100

    Added description scraper

--- indeed/IndeedScraper.py
+++ indeed/IndeedScraper.py
@@ -19,6 +19,66 @@ def get_title(title):
     return title
 
 
+def get_job_search(url, base_url, headers, verbose=True):
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Extract list of jobcards
+    jobs_list = soup.find(id='resultsCol')
+    jobcards = soup.find_all(class_='jobsearch-SerpJobCard')
+
+    # Loop through each job result listed on page
+    df_page = pd.DataFrame()
+    for j, card in enumerate(jobcards):
+        # if j > 1: break
+
+        card_title = card.find(class_='jobtitle')
+        card_title_str = card_title.text.strip()
+        card_url = base_url + card_title['href']
+
+        card_id = card['id']
+
+        card_company = card.find('span', class_='company')
+        card_company = card_company.text.strip()
+
+        card_loc = card.find(class_='location')
+        card_loc_str = card_loc.text.strip()
+
+        card_summary = card.find(class_='summary')
+        card_summary_str = card_summary.text.strip()
+
+        card_date = card.find(class_='date')
+        card_date_str = card_date.text.strip()
+
+        if verbose: print(f'job:{j:2} title:{card_title_str[:20]:20} company:{card_company}')
+
+        df_page = df_page.append({
+            'title': card_title_str,
+            'id': card_id,
+            'company': card_company,
+            'url': card_url,
+            'location': card_loc_str,
+            'summary': card_summary_str,
+            'date': card_date_str
+        }, ignore_index=True)
+        
+        
+    if df_page.empty:
+        print(jobs_list.prettify())
+    return df_page
+
+def get_job_description(url, headers):
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    description_html = soup.find(id='jobDescriptionText')
+    description_str = description_html.text.strip()
+    
+    descr = {'description':description_str,
+             'description_html':description_html}         
+    return descr
+
+
 class SearchPageScraper:
     base_url = 'https://uk.indeed.com'
     base_url_jobs = '/jobs?'
@@ -26,11 +86,18 @@ class SearchPageScraper:
     def __init__(self, title, loc):
         self.title = get_title(title)
         self.loc = loc
-        self.df = pd.DataFrame()
+        self.df = pd.DataFrame(columns=['title','id','company','url','location','summary',
+                                        'date', 'description', 'description_html'])
         self.url = f'{self.base_url}{self.base_url_jobs}q={self.title}&l={self.loc}'
         self.headers = {'Uer-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
         self.pages_scraped = 0
+        self.descriptions_scraped = 0
         
+    def new_search(self, title, loc):
+        self.title = get_title(title)
+        self.loc = loc
+        self.url = f'{self.base_url}{self.base_url_jobs}q={self.title}&l={self.loc}'
+
     def pre_check(self):
         print('Input validation not written yet')
     
@@ -56,47 +123,7 @@ class SearchPageScraper:
 
                 # Request page
                 time.sleep(1)
-                page = requests.get(url_page, headers=self.headers)
-                soup = BeautifulSoup(page.content, 'html.parser')
-
-                # Extract list of jobcards
-                jobs_list = soup.find(id='resultsCol')
-                jobcards = soup.find_all(class_='jobsearch-SerpJobCard')
-
-                # Loop through each job result listed on page
-                df_page = pd.DataFrame()
-                for j, card in enumerate(jobcards):
-                    # if j > 1: break
-
-                    card_title = card.find(class_='jobtitle')
-                    card_title_str = card_title.text.strip()
-                    card_url = self.base_url + card_title['href']
-
-                    card_id = card['id']
-
-                    card_company = card.find('span', class_='company')
-                    card_company = card_company.text.strip()
-
-                    card_loc = card.find(class_='location')
-                    card_loc_str = card_loc.text.strip()
-
-                    card_summary = card.find(class_='summary')
-                    card_summary_str = card_summary.text.strip()
-
-                    card_date = card.find(class_='date')
-                    card_date_str = card_date.text.strip()
-
-                    if verbose: print(f'job:{j:2} title:{card_title_str[:20]:20} company:{card_company}')
-
-                    df_page = df_page.append({
-                        'title': card_title_str,
-                        'id': card_id,
-                        'company': card_company,
-                        'url': card_url,
-                        'location': card_loc_str,
-                        'summary': card_summary_str,
-                        'date': card_date_str
-                    }, ignore_index=True)
+                df_page = get_job_search(url_page, self.base_url, self.headers, verbose=True)
 
                 if df_page.empty:
                     if verbose: print(f'Blank result, retrying ({attempt+1} of {attempts} attempts)')
@@ -106,45 +133,34 @@ class SearchPageScraper:
                     self.df = self.df.append(df_page, ignore_index=True)
                     break
             self.pages_scraped = self.pages_scraped + 1
-        return
+    
     
     def new_query(self, title, loc):
         self.title = get_title(title)
         self.loc = loc
         self.url = f'{self.base_url}{self.base_url_jobs}q={self.title}&l={self.loc}'
         
+        
     def drop_duplicates(self):
+        print(f'{len(self.df)} rows before dropping duplicates')
         self.df = self.df.drop_duplicates('id')
+        print(f'{len(self.df)} rows after dropping duplicates')
         
-class JobPageScraper:
-    
-    def __init__(self, url):
-        self.url = url
-        self.headers = {'Uer-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
-        self.data = None
 
-    def __repr__(self):
-        repr = f'JobPageScraper(url="{self.url}")'
-        return repr
-    
-    def scrape(self, attempts = 3, verbose=True):
-        data = self.data
-        # Try a dict instead
+    def add_descriptions(self):
+        if self.df.empty:
+            raise Exception('No existing scraped data.  First use .scrape() method')
         
-        # Retry if blank result
-        for attempt in range(attempts):
-            # if verbose: print(f'\nPage:{i} url:{url_page}')
-
-            # Request page
-            time.sleep(1)
-            page = requests.get(url, headers=self.headers)
-            soup = BeautifulSoup(page.content, 'html.parser')
-
-            if data is None:
-                if verbose: print(f'Blank result, retrying ({attempt+1} of {attempts} attempts)')
-                time.sleep(1)
+        for i, row in self.df.iterrows():                        
+            if not pd.isna(row.description):
+                print(f'row {i:2}, description already added, skipping')
                 continue
             else:
-                break
-        return
- 
+                time.sleep(1)
+                descr_dict = get_job_description(row.url, self.headers)
+                print(f'row {i:2}, description added ({len(descr_dict)} characters)')
+                
+                row.description = descr_dict['description']
+                row.description_html = descr_dict['description_html']
+                self.descriptions_scraped = self.descriptions_scraped+1
+            
--- script.py
+++ script.py
@@ -1,27 +1,7 @@
-from indeed.IndeedScraper import SearchPageScraper
-from indeed.IndeedScraper import JobPageScraper
+import indeed.IndeedScraper as ind
 
+scraper = ind.SearchPageScraper('Data Scientist', 'Scotland')
 
-# Create instance of class
-scraper = SearchPageScraper('Data Scientist', 'Scotland')
-
-# Print the object before
-print(scraper)
-
-# Run the scraper and print it
 scraper.scrape(2, verbose=True)
-print(scraper)
 
-# Try the JobPageScraper
-this_url = scraper.df['url'].iloc[0]
-job_scraper = JobPageScraper(this_url)
-print(job_scraper)
-
-# # Update the search query, run the scraper and print it
-# scraper.new_query('Data Analyst', 'Edinburgh')
-# scraper.scrape(2, verbose=True)
-# print(scraper)
-
-# # Remove duplicates
-# scraper.drop_duplicates()
-# print(scraper)
+# Debugging the blank pages
