818d317419edd092ae3f90a902a682b47e77d90a
Azam Din <adin786@gmail.com>
2021-07-01 01:44:28 +0100

    Added batch_scrape to query a bunch of jobsearches at once

---
+++ batch_scrape.py
@@ -0,0 +1,25 @@
+import indeed.IndeedScraper as ind
+
+# ind.batch_scrape([('Data Scientist', 'Scotland'),
+#                   ('Data Engineer', 'Scotland'),
+#                   ('Data Analyst', 'Scotland'),
+#                   ('Business Analyst', 'Scotland'),
+#                   ('Machine Learning Engineer', 'Scotland'),
+#                   ('Machine Learning', 'Scotland'),
+#                   ('Artificial Intelligence', 'Scotland'),
+#                   ('AI', 'Scotland'),
+#                   ('Data', 'Scotland')],
+#                  pages=20,
+#                  delay=5,
+#                  append=False)
+
+
+ind.batch_scrape([('Business Analyst', 'Scotland'),
+                  ('Machine Learning Engineer', 'Scotland'),
+                  ('Machine Learning', 'Scotland'),
+                  ('Artificial Intelligence', 'Scotland'),
+                  ('AI', 'Scotland'),
+                  ('Data', 'Scotland')],
+                 pages=20,
+                 delay=5,
+                 append=False)
